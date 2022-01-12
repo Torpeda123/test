d11260c4ac10d440f0f3620953a947895acb97a5
unknown <somov_oleg_2013@mail.ru>
2022-01-12 10:18:55 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$('.reviews-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+  $('.mainSlider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
